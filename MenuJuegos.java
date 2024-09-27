5462a0f9863196e88cfdafadb897ee2fb5e097c2
Juan Camilo García <juanca.cuevas.05@outlook.com>
2024-09-27 10:11:13 +0200

    Update MenuJuegos.java
    Cambios en los nombres de algunos archivos

--- MenuJuegos.java
+++ MenuJuegos.java
@@ -2,10 +2,10 @@ import javax.swing.JOptionPane;
 public class MenuJuegos {
     public static void main(String[] args) {
         // Mostramos el panel con las opciones de juegos
-        String[] opciones = { "Ahorcado", "Conecta 4", "Número Secreto", "Sudoku" };
+        String[] opciones = { "Ahorcado", "Conecta4", "Número Secreto"};
         String seleccion = (String) JOptionPane.showInputDialog(
                 null,
-                "¿Qué juego deseas jugar?",
+                "¿Qué juego deseas mi rey/reina?",
                 "Selección de Juego",
                 JOptionPane.QUESTION_MESSAGE,
                 null,
@@ -17,21 +17,18 @@ public class MenuJuegos {
                 case "Ahorcado":
                     Ahorcado.main(null);
                     break;
-                case "Conecta 4":
+                case "Conecta4":
                     Conecta4.main(null);
                     break;
-                case "Número Secreto":
-                    numeroSecreto.main(null);
-                    break;
-                case "Sudoku":
-                    Sudoku.main(null);
+                case "AdivinaNumero":
+                    AdivinaNumero.main(null);
                     break;
                 default:
-                    JOptionPane.showMessageDialog(null, "Opción no válida.");
+                    JOptionPane.showMessageDialog(null, "Opción no válida, escoge bien .I. ");
                     break;
             }
         } else {
-            JOptionPane.showMessageDialog(null, "No has seleccionado ningún juego.");
+            JOptionPane.showMessageDialog(null, "No has seleccionado ningún juego, ¿ a caso no quieres jugar?");
         }
     }
 }
